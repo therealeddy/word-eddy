4549f853220e906046fdff1b21a915ccfac01003
therealeddy <eduardo.mfonseca@hotmail.com>
2020-01-08 18:44:58 -0300

    gravity forms

---
+++ mu-plugins/gravityforms.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name: Gravity Forms
+Plugin URI: https://www.gravityforms.com
+Description: Easily create web forms and manage form entries within the WordPress admin.
+Version: 2.3.3.10
+Author: rocketgenius
+Author URI: https://www.rocketgenius.com
+License: GPL-2.0+
+Text Domain: gravityforms
+Domain Path: /languages
+
+------------------------------------------------------------------------
+Copyright 2009-2018 Rocketgenius, Inc.
+*/
+
+require(WPMU_PLUGIN_DIR .'/gravityforms/gravityforms.php');
